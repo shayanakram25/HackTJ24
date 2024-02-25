3dacefaa2638b8819965cc1ffaf6271b9d52e383
skrocket <122058962+skrocket@users.noreply.github.com>
2024-02-25 07:25:14 -0500

    Update app.py

--- Hack TJ Website/app.py
+++ Hack TJ Website/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, jsonify
 import openai
 
 app = Flask(__name__)
-openai.api_key = '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'
+openai.api_key = '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'
 
 
 @app.route('/')
